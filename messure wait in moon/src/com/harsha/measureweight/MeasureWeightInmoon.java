60f32779f86b74d029da6a6fea0173220f3092e8
hathamunage harsha sandaruwan <sandaruwanharsha02@gmail.com>
2020-12-03 11:57:15 +0530

    calculation of weight in moon

---
+++ messure wait in moon/src/com/harsha/measureweight/MeasureWeightInmoon.java
@@ -0,0 +1,12 @@
+package com.harsha.measureweight;
+
+public class MeasureWeightInmoon {
+    public static void main(String[] arg){
+        double myWeight_E = 105;
+        double pacentage = 0.17 ;
+
+        double myWeight_M = myWeight_E * pacentage;
+
+        System.out.println("My weight in moon is " + myWeight_M);
+    }
+}
